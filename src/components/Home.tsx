bd954cae9b4679ba24bf533a3cb22714aad5141f
Sarmad Rafique <sarmadrafique040@gmail.com>
2025-02-15 20:47:30 +0500

    Refactor Home component to organize links into sections for better clarity and add a new section for React 19 concepts

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,17 +5,36 @@ export default function Home() {
     "flex items-center justify-center bg-black text-white px-4 py-2 rounded-md h-[45px] w-[150px]";
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h3 className="text-3xl md:text-4xl font-semibold mb-5">Hooks</h3>
-      <div className="flex gap-6 flex-wrap">
-        <Link to="/hooks/useContext" className={linkStyles}>
-          useContext
-        </Link>
-        <Link to="/hooks/useTransition" className={linkStyles}>
-          useTransition
-        </Link>
-        <Link to="/hooks/useRef" className={linkStyles}>
-          useRef
-        </Link>
+      <div className="my-5">
+        <h3 className="text-3xl md:text-4xl font-semibold mb-5">Hooks</h3>
+        <div className="flex gap-6 flex-wrap">
+          <Link to="/hooks/useContext" className={linkStyles}>
+            useContext
+          </Link>
+          <Link to="/hooks/useRef" className={linkStyles}>
+            useRef
+          </Link>
+        </div>
+      </div>
+      <div className="my-8">
+        <h3 className="text-3xl md:text-4xl font-semibold mb-5">
+          React 19 Concepts
+        </h3>
+        <div className="flex gap-6 flex-wrap">
+          <Link
+            to="https://react.dev/blog/2024/12/05/react-19"
+            target="_blank"
+            className={linkStyles}
+          >
+            View Docs
+          </Link>
+          <Link to="/hooks/useTransition" className={linkStyles}>
+            useTransition
+          </Link>
+          <Link to="/hooks/useRef" className={linkStyles}>
+            useRef
+          </Link>
+        </div>
       </div>
     </div>
   );
