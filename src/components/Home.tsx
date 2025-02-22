f69fd8c7ce523f86903291e519e256d63e51cfa3
Sarmad Rafique <sarmadrafique040@gmail.com>
2025-02-22 10:25:58 +0500

    Refactor Home component to use standardized link styles for improved consistency

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,17 +1,15 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
-  const linkStyles =
-    "flex items-center justify-center bg-black text-white px-4 py-2 rounded-md h-[45px] w-[150px]";
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="my-5">
         <h3 className="text-3xl md:text-4xl font-semibold mb-5">Hooks</h3>
         <div className="flex gap-6 flex-wrap">
-          <Link to="/hooks/useContext" className={linkStyles}>
+          <Link to="/hooks/useContext" className="link-styles">
             useContext
           </Link>
-          <Link to="/hooks/useRef" className={linkStyles}>
+          <Link to="/hooks/useRef" className="link-styles">
             useRef
           </Link>
         </div>
@@ -24,11 +22,11 @@ export default function Home() {
           <Link
             to="https://react.dev/blog/2024/12/05/react-19"
             target="_blank"
-            className={linkStyles}
+            className="link-styles"
           >
             View Docs
           </Link>
-          <Link to="/hooks/useTransition" className={linkStyles}>
+          <Link to="/hooks/useTransition" className="link-styles">
             useTransition
           </Link>
         </div>
