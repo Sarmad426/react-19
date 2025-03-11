7a927cc6c6ebca4f9db09845bc736729ba739cd5
Sarmad Rafique <sarmadrafique040@gmail.com>
2025-03-11 12:02:08 +0500

    Wrap App component in QueryClientProvider and add SearchProject route

--- src/App.tsx
+++ src/App.tsx
@@ -14,35 +14,57 @@ import { Practice } from "./components/practice/practice";
 import { UseOptimisticHook } from "./components/react19-concepts/useOptimistic";
 import { UseFormStatusHook } from "./components/react19-concepts/useFormStatus";
 import { UseAPI } from "./components/react19-concepts/use";
+import SearchProject from "./components/search-input-project/project-main";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/hooks/useTransition" element={<UseTransitionLinks />} />
-          <Route
-            path="/hooks/useTransition/tabs"
-            element={<UseTransitionTabs />}
-          />
-          <Route
-            path="/hooks/useTransition/form"
-            element={<UseTransitionForm />}
-          />
-          <Route path="/hooks/useContext" element={<UseContextExample />} />
-          <Route path="/hooks/useRef" element={<UseRefExample />} />
-          <Route path="/hooks/useActionState" element={<UseActionState />} />
-          <Route path="/hooks/useOptimistic" element={<UseOptimisticHook />} />
-          <Route path="/hooks/useOptimistic" element={<UseOptimisticHook />} />
-          <Route path="/hooks/useFormStatus" element={<UseFormStatusHook />} />
-          <Route path="/API/use" element={<UseAPI />} />
-          <Route path="/practice" element={<Practice />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/hooks/useTransition"
+              element={<UseTransitionLinks />}
+            />
+            <Route
+              path="/hooks/useTransition/tabs"
+              element={<UseTransitionTabs />}
+            />
+            <Route
+              path="/hooks/useTransition/form"
+              element={<UseTransitionForm />}
+            />
+            <Route path="/hooks/useContext" element={<UseContextExample />} />
+            <Route path="/hooks/useRef" element={<UseRefExample />} />
+            <Route path="/hooks/useActionState" element={<UseActionState />} />
+            <Route
+              path="/hooks/useOptimistic"
+              element={<UseOptimisticHook />}
+            />
+            <Route
+              path="/hooks/useOptimistic"
+              element={<UseOptimisticHook />}
+            />
+            <Route
+              path="/hooks/useFormStatus"
+              element={<UseFormStatusHook />}
+            />
+            <Route path="/API/use" element={<UseAPI />} />
+            <Route path="/practice" element={<Practice />} />
+            <Route
+              path="/projects/search-input-project"
+              element={<SearchProject />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </Router>
+    </QueryClientProvider>
   );
 };
 
